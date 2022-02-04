9b47623f61ec09517552a4c0a34dd530693aecc0
BlueTompon <lucastran00@gmail.com>
2022-02-04 16:08:59 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ListArticleComponent } from './list-article/list-article.component';
+import { DisplayArticleComponent } from './display-article/display-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListArticleComponent,
+    DisplayArticleComponent
   ],
   imports: [
     BrowserModule,
